f64d116577e6b7ebb978167283b65ba1269bc3e9
arishorts <arieljschwartz@gmail.com>
2023-03-28 10:55:25 -0700

    Establishing folder setup part 2

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
+const path = require('path');
+//const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -9,7 +11,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({});
-app.use('view engine', hbs);
+// Create the Handlebars.js engine object with custom helper functions
+// const hbs = exphbs.create({ helpers });
+
+// Inform Express.js which template engine we're using
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 const sess = {
   secret: 'Super secret secret',
@@ -25,6 +32,7 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
