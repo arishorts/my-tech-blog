1046f4411a6db5453714d4d95fdd0489890b4275
arishorts <arieljschwartz@gmail.com>
2023-03-28 15:19:04 -0700

    Added logic for checking paswords and email

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const apiRoutes = require('./api');
+const loginRoutes = require('./loginRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/login', loginRoutes);
 router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
