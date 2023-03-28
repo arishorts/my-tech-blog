9a9782574c89905780040e38a9a2dd5ec9277c01
arishorts <arieljschwartz@gmail.com>
2023-03-28 11:55:29 -0700

    successfully rendered all blogposts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
+const BlogPost = require('../models/BlogPost');
 
 router.get('/', async (req, res) => {
-  // Send the rendered Handlebars.js template back as the response
-  res.render('homepage');
+  try {
+    const blogPostData = await BlogPost.findAll();
+    if (!blogPostData) {
+      res.status(404).json({ message: 'no data found' });
+    }
+    const blogposts = blogPostData.map((blogpost) =>
+      blogpost.get({ plain: true })
+    );
+    res.render('homepage', { blogposts });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
